4481fff056f259841a0c30ae5c5a98e91a8d707b
HerrNamenlos123 <herrnamenlos123@gmail.com>
2021-12-18 18:09:40 +0100

    Update

--- scripts/generate-allegro5.bat
+++ scripts/generate-allegro5.bat
@@ -9,8 +9,8 @@ cd "%~dp0..\modules\allegro5\build"
 
 cmake .. -Wno-dev -DPREFER_STATIC_DEPS=true -DSHARED=false -DWANT_DOCS=false -DWANT_DOCS_HTML=false ^
 -DWANT_EXAMPLES=false -DWANT_FONT=true -DWANT_MONOLITH=true -DWANT_TESTS=false -DWANT_RELEASE_LOGGING=false ^
--DWANT_STATIC_RUNTIME=true -DFREETYPE_INCLUDE_DIRS=../../freetype/src ^
--DFREETYPE_LIBRARIES=../../freetype/build/Release/freetype.lib
+-DWANT_STATIC_RUNTIME=true -DFREETYPE_INCLUDE_DIRS=../freetype/src;../freetype/include ^
+-DFREETYPE_LIBRARIES=../freetype/build/Release/freetype.lib
 
 if %errorlevel% neq 0 echo [91mCMake was unsuccessful[0m & set errorlevel=0 & goto :eof
 
